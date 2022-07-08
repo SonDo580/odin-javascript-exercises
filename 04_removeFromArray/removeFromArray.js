3ed3de392cae60a986c3b7a0743a7420338e136f
Son Do <dominhson580@gmail.com>
2022-07-08 22:51:13 +0700

    Passed 2 first tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,22 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...values) {
+    const resultArray = [];
 
+    for (let i = 0; i < array.length; i++) {
+        let removed = false;
+
+        for (let j = 0; j < values.length; j++) {
+            if (array[i] === values[j]) {
+                removed = true;
+                break;
+            } 
+        }
+
+        if (!removed) {
+            resultArray.push(array[i]);
+        }
+    }
+
+    return resultArray;
 };
 
 // Do not edit below this line
