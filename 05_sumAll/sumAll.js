9e30cdfbc82a66c3899c1c38d018083b8878aee2
Son Do <dominhson580@gmail.com>
2022-07-08 23:10:56 +0700

    Exercise 5: Passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+    if (!(typeof a === 'number' && typeof b === 'number')
+        || a === NaN
+        || b === NaN
+        || a < 0
+        || b < 0
+        || Math.floor(a) !== a
+        || Math.floor(b) !== b) {
+            return 'ERROR';
+        }
+    
+    let sum = 0;
+    
+    if (a === b) {
+        return 2 * a;
+    } else if (a < b) {
+        for (let i = a; i <= b; i++) {
+            sum += i;
+        }
+    } else {
+        for (let i = b; i <= a; i++) {
+            sum += i;
+        }
+    }
 
+    return sum;
 };
 
 // Do not edit below this line
