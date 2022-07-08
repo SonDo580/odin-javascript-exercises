ab45152d30708dd74dd6574bbbe12a339fa36248
Son Do <dominhson580@gmail.com>
2022-07-08 14:12:02 +0700

    Exercise 3: Passed all the tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    const arrayChar = string.split('');
+    const reverseArrayChar = [];
 
+    for (let i = string.length - 1; i >= 0; i--) {
+        reverseArrayChar.push(arrayChar[i]);
+    }
+
+    return reverseArrayChar.join('');
 };
 
 // Do not edit below this line
