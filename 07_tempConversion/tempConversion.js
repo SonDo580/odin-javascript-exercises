5ba8f73aa7117104c2164e917a73b131ef42a300
Son Do <dominhson580@gmail.com>
2022-07-08 23:29:55 +0700

    Exercise 7: Passed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,14 @@
-const ftoc = function() {
-
+const ftoc = function(fdegree) {
+  return roundToOneDecimal(5 * (fdegree - 32) / 9);
 };
 
-const ctof = function() {
-
+const ctof = function(cdegree) {
+  return roundToOneDecimal(32 + 9 * cdegree / 5);
 };
+
+function roundToOneDecimal(num) {
+  return Math.round(num * 10) / 10;
+}
 
 // Do not edit below this line
 module.exports = {
