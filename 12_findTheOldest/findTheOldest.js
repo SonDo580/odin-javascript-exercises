ec2c6b993664fad673216a99347b366db7a2946a
Son Do <dominhson580@gmail.com>
2022-07-13 23:28:59 +0700

    Passed all the tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+    let oldest = people[0];
 
+    for (let person of people) {
+        if (findAge(person) > findAge(oldest)) {
+            oldest = person;
+        }
+    }
+
+    return oldest;
 };
+
+function findAge(person) {
+    if (!('yearOfDeath' in person)) {
+        
+        return new Date().getFullYear() - person.yearOfBirth;
+    }
+
+    return person.yearOfDeath - person.yearOfBirth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
