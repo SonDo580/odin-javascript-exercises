8ba391a09fcb4405344bbe2b7a615b87335e6b9e
Son Do <dominhson580@gmail.com>
2022-07-13 22:25:43 +0700

    Passed all the tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (!['number', 'string'].includes(typeof num)
+        || +num === NaN
+        || num <= 0
+        || Math.floor(+num) !== +num
+        ) 
+    {
+        return 'OOPS';
+    }
 
+    num = +num;
+    
+    if (num === 1 || num === 2) {
+        return 1;
+    }
+
+    return fibonacci(num - 1) + fibonacci(num - 2);
 };
 
 // Do not edit below this line
