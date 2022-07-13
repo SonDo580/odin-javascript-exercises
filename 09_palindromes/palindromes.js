7e84aeea27390ae9546b0fc596576d1964e6043f
Son Do <dominhson580@gmail.com>
2022-07-13 21:58:44 +0700

    Passed all the tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    // Remove all punctuations and spaces
+    let alphabetString = string.replace(/[^A-Za-z]+/g, '');
 
+    let reversedString = alphabetString.split('').reverse().join('');
+
+    if (reversedString.toLowerCase() !== alphabetString.toLowerCase()) {
+        return false;
+    }
+    
+    return true;
 };
 
 // Do not edit below this line
